a804f1fa7f1c2132d3e248ab19233fe27e08dcbb
ghostry <ghostry@ghostry.cn>
2016-01-01 00:13:11 +0800

    add weather

--- weather.sh
+++ weather.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-url="http://dy.ghostry.cn/Home/index/weather/string/1/area/CHXX0072";
+#地区编码
+code="CHXX0072";
+
+url="http://dy.ghostry.cn/Home/index/weather/string/1/area/$code";
 date=$(export LANG=en_US;stat /tmp/weather.cache |grep Modify|awk -F ':' '{print $2}'|awk '{printf("%s %s",$1,$2)}'|awk -F "." '{print $1}');
 time=$(date -d "$date" +%s);
 time=$[$time + 3600];
