9addefff1e2d43e316264d82ba45874a9a03ec5b
ghostry <ghostry@ghostry.cn>
2016-01-01 00:26:14 +0800

    add weather

--- weather.sh
+++ weather.sh
@@ -2,12 +2,17 @@
 #地区编码
 code="CHXX0072";
 
+cachefile=/tmp/weather.cache;
 url="http://dy.ghostry.cn/Home/index/weather/string/1/area/$code";
-date=$(export LANG=en_US;stat /tmp/weather.cache |grep Modify|awk -F ':' '{print $2}'|awk '{printf("%s %s",$1,$2)}'|awk -F "." '{print $1}');
-time=$(date -d "$date" +%s);
-time=$[$time + 3600];
-if [ $time -lt $(date +%s) ] ;then
-        `wget -O- $url 2>/dev/null 1>/tmp/weather.cache`
+date=$(export LANG=en_US;stat $cachefile |grep Modify|awk -F ':' '{print $2}'|awk '{printf("%s %s",$1,$2)}'|awk -F "." '{print $1}');
+if [ ! $date ] ;then
+        `wget -O- $url >$cachefile`
+else
+        time=$(date -d "$date" +%s);
+        time=$[$time + 3600];
+        if [ $time -lt $(date +%s) ] ;then
+                `wget -O- $url >$cachefile`
+        fi
 fi
 weather=$(cat /tmp/weather.cache);
 if [ "$1" -eq "1" ] ;then
