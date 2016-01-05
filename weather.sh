74a716ccf39fbcc3804df89d5ca0e78420a98a3d
ghostry <ghostry@ghostry.cn>
2016-01-05 19:11:00 +0800

    update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #地区编码
 code="CHXX0072";
+#更新频率，单位：秒
+interval=3600;
 
 cachefile=/tmp/weather.cache;
 url="http://dy.ghostry.cn/Home/index/weather/string/1/area/$code";
 date=$(export LANG=en_US;stat $cachefile |grep Modify|awk -F ':' '{print $2}'|awk '{printf("%s %s",$1,$2)}'|awk -F "." '{print $1}');
-if [ ! $date ] ;then
+if [ ! "$date" ] ;then
         `wget -O- $url >$cachefile`
 else
         time=$(date -d "$date" +%s);
-        time=$[$time + 3600];
+        time=$[$time + $interval];
         if [ $time -lt $(date +%s) ] ;then
                 `wget -O- $url >$cachefile`
         fi
